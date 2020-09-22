58d80420f7560da34758fbd133c1f4f2fc9c6c2a
VladislavKupriyanov <kupriyanov6308@gmail.com>
2020-09-23 02:46:43 +0300

    part 1

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a1 = prompt('Один из последних просмотренных фильмов', '');
+const a2 = prompt('На сколько оцените его?', '');
+const b1 = prompt('Один из последних просмотренных фильмов', '');
+const b2 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a1] = a2;
+personalMovieDB.movies[b1] = b2;
+
+console.log(personalMovieDB);
